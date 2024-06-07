d93583ddaa1fdd2f417616c13303362de271d511
Amir Arsalan Taqavi <amirarsalantaqavi@gmail.com>
2024-06-08 01:09:29 +0330

    1

---
+++ lib/services/auth/firebase_auth_provoder.dart
@@ -0,0 +1,5 @@
+import 'package:mynotes/services/auth/auth_user.dart';
+import 'package:mynotes/services/auth/auth_provider.dart';
+import 'package:mynotes/services/auth/auth_execeptions.dart';
+
+// import 'package:firebase_auth/firebase_auth.dart' show FirebaseAuthProvider, implements FirebaseAuthException;
